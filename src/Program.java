66da870a4389630d37cd829e04385270c5c4ccd6
liranzxc <36790388+liranzxc@users.noreply.github.com>
2018-09-19 23:49:03 +0300

    Add files via upload

--- src/Program.java
+++ src/Program.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,6 +11,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import net.openhft.hashing.LongHashFunction;
@@ -21,14 +23,14 @@ public class Program {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
-		long StartTime = System.currentTimeMillis();
-		String dir = "C:\\Users\\liran\\Desktop\\google-images-download\\google_images_download\\downloads\\Apple and Banana and Elephant";
+		long StartTime = System.nanoTime();
+		String dir = "B:\\All_Data_To_Scan";
 		name_key = Files.walk(Paths.get(dir)).parallel().filter(f -> f.toFile().isFile())
 				.filter(f -> f.toString().endsWith("jpg") || f.toString().endsWith("jpeg")
-						|| f.toString().endsWith("gif") || f.toString().endsWith("png"))
-				.collect(Collectors.toConcurrentMap(Path::toAbsolutePath, p -> GetHashImage(p)));
+						|| f.toString().endsWith("gif") || f.toString().endsWith("png") || f.toString().endsWith("JPG"))
+				.collect(Collectors.toConcurrentMap(Path::toAbsolutePath, p -> GetHashImage_Sample(p)));
 
-		List<Long> list_of_all_keys_more_than_one = name_key.values().parallelStream()
+		List<Long> list_of_all_keys_more_than_one = name_key.values().parallelStream().filter(v -> v != 0)
 				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().parallelStream()
 				.filter(v -> v.getValue() > 1).map(Map.Entry::getKey).collect(Collectors.toList());
 
@@ -43,15 +45,15 @@ public class Program {
 					.map(Entry::getKey).get());
 
 		});
-		long endTime = System.currentTimeMillis();
+		long endTime = System.nanoTime();
 		
-		for(Path path : pathtoDeleted)
-		{
-			System.out.println(path.getFileName() + " Deleted");
-			Files.deleteIfExists(path);
-		}
 
-		System.out.println("time to process " + (endTime - StartTime));
+		for (Path path : pathtoDeleted) {
+			System.out.println(path.getFileName() + " Deleted");
+			// Files.deleteIfExists(path);
+
+		}
+		System.out.println("time to process " + (TimeUnit.NANOSECONDS.toSeconds(endTime - StartTime) + " Seconds"));
 
 	}
 
@@ -65,8 +67,23 @@ public class Program {
 			random.close();
 			return LongHashFunction.xx().hashBytes(b);
 		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
+			// e.printStackTrace();
+		}
+		return 0;
+//-7392529867172308720
+//-7392529867172308720
+	}
+
+	private static long GetHashImage_Sample(Path f) {
+		try {
+			File file = f.toFile();
+			RandomAccessFile random = new RandomAccessFile(file, "r");
+			byte[] b = new byte[1024];
+			random.readFully(b);
+			random.close();
+			return LongHashFunction.xx().hashBytes(b);
+		} catch (Exception e) {
+			 e.printStackTrace();
 		}
 		return 0;
 //-7392529867172308720
